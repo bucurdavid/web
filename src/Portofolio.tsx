457890e470a4dd97017ffe02f18aac99b7d451ee
bucurdavid <dbucur22@gmail.com>
2025-01-21 22:23:10 +0200

    fix: lint

--- src/Portofolio.tsx
+++ src/Portofolio.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import { Monitor, Code, Mail, Github, Linkedin } from "lucide-react";
 import GitHubActivity from "./GithubActivity";
 
